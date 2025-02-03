4bca08680e2b116bd4921369df9eb652c1065288
siarhei.Kovalkov <s.kovalkov@lacit.net>
2025-02-03 13:11:13 +0300

    no message

--- BookShop/src/constants/apiConstants.ts
+++
@@ -1,8 +0,0 @@
-export const NEW_BOOK: string = "https://api.itbook.store/1.0/new";
-export const SEARCH_BOOK: string = "https://api.itbook.store/1.0/search/@query/@page";
-export const CURRENT_BOOK: string = "https://api.itbook.store/1.0/books/@iban";
-
-export const C_CHARP_BOOKS: string = SEARCH_BOOK.replace("@query", "sharp");
-export const JAVASCRIPT_BOOKS: string = SEARCH_BOOK.replace("@query", "javascript");
-export const SQL_BOOKS: string = SEARCH_BOOK.replace("@query", "sql");
-export const DATABASE_BOOKS: string = SEARCH_BOOK.replace("@query", "database");
